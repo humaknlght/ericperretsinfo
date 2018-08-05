501aca421573b939026aeed5fb951f6e63444ae8
Eric <perree@cd-a3-ee-11-95-a3-4a-ab-c5.local>
2018-08-05 01:45:27 -0700

    Initial Checkin
                                        ,$$.       ,$$.
                                       ,$'`$.     ,$'`$.
                                       $'  `$     $'  `$
                                      :$    $;   :$    $;
                                      $$    $$   $$    $$
                                      $$  _.$bqgpd$._  $$
                                      ;$gd$$^$$$$$^$$bg$:
                                    .d$P^*'   "*"   `*^T$b.
                                   d$$$    ,*"   "*.    $$$b
                                  d$$$b._    o   o    _.d$$$b
                                 *T$$$$$P             T$$$$$P*
                                   `^T$$    :"---";    $$P^'
                                      `$._   `---'   _.$'
                                     .d$$P"**-----**"T$$b.
                                    d$$P'             `T$$b
                                   d$$P                 T$$b
                                  d$P'.'               `.`T$b
                                  `--:                   ;--'
                                     |                   |
                                     :                   ;
                                      \                 /
                                      .`-.           .-'.
                                     /   ."*--+g+--*".   \
                                    :   /     $$$     \   ;
                                    `--'      $$$      `--'
                                              $$$ [bug]
                                              $$$
                                              :$$;
                                              :$$;
                                               :$$
                                               'T$bg+.____
                                                 'T$$$$$  :
                                                     "**--'

---
+++ build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp ../src ./prod 
+shopt -s nullglob
+for FILE in ./prod/*.{html,css} ./prod/resume/*.{html,svg,pdf}
+do
+    echo "$FILE"
+    ./zopfli "$FILE"
+    rm "$FILE.br"
+    brotli "$FILE"
+done
---
+++ src/img/jpg.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gzip -c -9 meme.jpg > meme.jpg.gz
