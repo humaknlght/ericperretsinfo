3b97853162955df670abccb6dd9acfc6ee2aacab
Eric <perree@cd-a3-ee-11-95-a3-4a-ab-c5.local>
2018-09-09 14:08:27 -0700

    Faster building.

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cp -r ../src ./prod 
+
+rm -rf ./prod
+mkdir ./prod
+cp -r ../src/* ./prod/
 shopt -s nullglob
 for FILE in ./prod/*.{html,css} ./prod/resume/*.{html,svg,pdf}
 do
     echo "$FILE"
     ./zopfli "$FILE"
-    rm "$FILE.br"
     brotli "$FILE"
 done
