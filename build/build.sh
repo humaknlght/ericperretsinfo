94d1c1c82328e2515104d8b5db55e5556f35bd9e
Eric <perree@cd-a3-ee-11-95-a3-4a-ab-c5.local>
2018-09-11 08:59:10 -0700

    Removed the backup file

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,7 @@ mkdir ./prod
 cp -r ../src/* ./prod/
 NUM=`stat -f "%Sm" -t "%s" ../src/resume/index.html`"000"
 sed -i .bk "s:/\*LAST_MODIFIED\*/:$NUM:" ./prod/resume/index.html
+rm -f ./prod/resume/index.html.bk
 shopt -s nullglob
 for FILE in ./prod/*.{html,css} ./prod/resume/*.{html,svg,pdf}
 do
