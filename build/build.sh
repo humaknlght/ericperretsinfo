0aafe7727f7470a90c2be98b572190f0c1d4de37
Eric <perree@cd-a3-ee-11-95-a3-4a-ab-c5.local>
2018-09-11 08:56:43 -0700

    Fixed the resume last modified so that it is generated at compile time

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,8 @@
 rm -rf ./prod
 mkdir ./prod
 cp -r ../src/* ./prod/
+NUM=`stat -f "%Sm" -t "%s" ../src/resume/index.html`"000"
+sed -i .bk "s:/\*LAST_MODIFIED\*/:$NUM:" ./prod/resume/index.html
 shopt -s nullglob
 for FILE in ./prod/*.{html,css} ./prod/resume/*.{html,svg,pdf}
 do
