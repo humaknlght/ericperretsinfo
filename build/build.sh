fa44f9d22aa643954dd958c81d864bef535c0301
Eric <perree@cd-a3-ee-11-95-a3-4a-ab-c5.local>
2018-09-29 12:10:37 -0700

    added support to minify ther css prior to compressing it

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,16 @@ NUM=`stat -f "%Sm" -t "%s" ../src/resume/index.html`"000"
 sed -i .bk "s:/\*LAST_MODIFIED\*/:$NUM:" ./prod/resume/index.html
 rm -f ./prod/resume/index.html.bk
 shopt -s nullglob
+#npm install uglifycss -g
+for FILE in ./prod/*.css
+do
+    echo "uglify ${FILE} to ${FILE}.min"
+    uglifycss "$FILE" > "${FILE}.min"
+    #workaroiund for bug in uglifycss
+    sed -i .bk "s: or(: or (:" "${FILE}.min"
+    rm -f "${FILE}.min.bk"
+    mv "${FILE}.min" "$FILE"
+done
 for FILE in ./prod/*.{html,css} ./prod/resume/*.{html,svg,pdf}
 do
     echo "$FILE"
