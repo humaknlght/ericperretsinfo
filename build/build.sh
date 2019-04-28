39bada2ea61439ef9ecc72b3ec5bfc7deac9db45
humaknlght <perree@gmail.com>
2019-04-28 16:48:11 -0700

    Add Babel support

--- build/build.sh
+++ build/build.sh
@@ -26,6 +26,14 @@ do
     html-minifier --keep-closing-slash --remove-comments --collapse-whitespace --minify-js --minify-css --decode-entities --no-html5 --process-conditional-comments --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --sort-attributes --use-short-doctype --trim-custom-fragments -o "${FILE}.min" "${FILE}"
     mv "${FILE}.min" "${FILE}"
 done
+#npm install @babel/core @babel/cli -g
+#npm install babel-preset-minify -g
+for FILE in ./prod/*.js
+do
+    echo "Minifying ${FILE} to ${FILE}.min"
+    babel "${FILE}" --presets minify > "${FILE}.min"
+    mv "${FILE}.min" "${FILE}"
+done
 for FILE in ./prod/*.{html,css,js} ./prod/resume/*.{html,svg,pdf} ./prod/art/*.html
 do
     echo "$FILE"
