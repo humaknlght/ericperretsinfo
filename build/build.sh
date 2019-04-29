338c4f08b67aa42b5223de16cb4fed85f78ea35a
humaknlght <perree@gmail.com>
2019-04-28 18:04:05 -0700

    CSP tweak

--- build/build.sh
+++ build/build.sh
@@ -10,7 +10,7 @@ sed -i .bk "s:/\*LAST_MODIFIED\*/:$NUM:" ./prod/resume/index.html
 rm -f ./prod/resume/index.html.bk
 shopt -s nullglob
 #npm install uglifycss -g
-for FILE in ./prod/*.css
+for FILE in ./prod/*.css ./prod/resume/.css
 do
     echo "uglify ${FILE} to ${FILE}.min"
     uglifycss "$FILE" > "${FILE}.min"
@@ -34,7 +34,7 @@ do
     babel "${FILE}" --presets minify > "${FILE}.min"
     mv "${FILE}.min" "${FILE}"
 done
-for FILE in ./prod/*.{html,css,js} ./prod/resume/*.{html,svg,pdf} ./prod/art/*.html
+for FILE in ./prod/*.{html,css,js} ./prod/resume/*.{html,svg,pdf} ./prod/resume/.css ./prod/art/*.html
 do
     echo "$FILE"
     ./zopfli "$FILE"
