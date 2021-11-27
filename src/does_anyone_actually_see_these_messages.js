bc244fe12b946a29e8c7acfb24634a1a542d9d1b
humaknlght <perree@gmail.com>
2021-11-26 23:54:39 -0800

    fix: correct google analytics

--- src/does_anyone_actually_see_these_messages.js
+++ src/does_anyone_actually_see_these_messages.js
@@ -174,7 +174,8 @@
             getAndLoadArt();
         }
     });
-    window.ga=function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-    ga("create","UA-39357422-1","auto");
-    ga("send","pageview");
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-39357422-1');
 }
