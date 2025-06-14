f33624c83bd4020f33c980a5ff3b31a4c9c173ff
humaknlght <perree@gmail.com>
2025-06-14 09:40:56 -0700

    feat: new css minifier

--- build/build.sh
+++ build/build.sh
@@ -9,16 +9,17 @@ NUM=`stat -f "%Sm" -t "%s" ../src/resume/index.html`"000"
 sed -i .bk "s:/\*LAST_MODIFIED\*/:$NUM:" ./prod/resume/index.html
 rm -f ./prod/resume/index.html.bk
 shopt -s nullglob
-#npm install uglifycss -g
+
+#npm install cssnano postcss --save-dev
+#npm install --save-dev postcss-cli
 for FILE in ./prod/*.css ./prod/resume/*.css
 do
-    echo "uglify ${FILE} to ${FILE}.min"
-    uglifycss "$FILE" > "${FILE}.min"
+    echo "postcss $FILE > ${FILE}.min"
+    npx postcss --verbose --no-map "$FILE" > "${FILE}.min"
     #workaround for bug in uglifycss
-    sed -i .bk "s: or(: or (:" "${FILE}.min"
-    rm -f "${FILE}.min.bk"
     mv "${FILE}.min" "$FILE"
 done
+
 #npm install html-minifier -g
 for FILE in ./prod/*.html ./prod/resume/*.html #./prod/art/*.html
 do
@@ -26,6 +27,7 @@ do
     html-minifier --keep-closing-slash --remove-comments --collapse-whitespace --minify-js --minify-css --decode-entities --no-html5 --process-conditional-comments --remove-redundant-attributes --remove-script-type-attributes --remove-style-link-type-attributes --use-short-doctype --trim-custom-fragments -o "${FILE}.min" "${FILE}"
     mv "${FILE}.min" "${FILE}"
 done
+
 #npm install @babel/core @babel/cli -g
 #npm install babel-preset-minify -g
 for FILE in ./prod/*.js
@@ -34,6 +36,7 @@ do
     babel "${FILE}" --presets minify > "${FILE}.min"
     mv "${FILE}.min" "${FILE}"
 done
+
 # https://github.com/google/brotli
 # https://github.com/google/zopfli
 for FILE in ./prod/*.{html,css,js} ./prod/resume/*.{html,svg,pdf,css} ./prod/art/*.html
